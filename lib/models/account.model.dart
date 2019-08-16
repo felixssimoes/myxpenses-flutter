55c0b2b827ad458ed86eccd1adfaacfddd204aa9
Félix Simões <felixssimoes@gmail.com>
2019-08-16 20:59:25 +0200

    adds saving accounts using shared preferences

--- lib/models/account.model.dart
+++ lib/models/account.model.dart
@@ -9,6 +9,15 @@ class Account {
     @required this.name,
   });
 
+  Account.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+      };
+
   Account copyWith({String name}) => Account(
         id: this.id,
         name: name ?? this.name,
--- lib/providers/accounts.provider.dart
+++ lib/providers/accounts.provider.dart
@@ -1,10 +1,23 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
 import 'package:myxpenses/models/account.model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const _accountsPreferenceKey = 'com.myxpenses.accounts';
 
 class AccountsProvider with ChangeNotifier {
   List<Account> _accounts = [];
 
   List<Account> get accounts => [..._accounts];
+
+  AccountsProvider() {
+    loadAccounts();
+  }
+
+  Account findById(String id) {
+    return _accounts.firstWhere((a) => a.id == id, orElse: () => null);
+  }
 
   Future<Account> addAccount({@required String name}) async {
     final newAccount = Account(
@@ -12,22 +25,36 @@ class AccountsProvider with ChangeNotifier {
       name: name,
     );
     _accounts.add(newAccount);
+    await _saveAccounts();
     notifyListeners();
     return newAccount;
   }
 
-  void updateAccount(Account account) {
+  Future<void> updateAccount(Account account) async {
     final index = _accounts.indexWhere(((a) => a.id == account.id));
     _accounts[index] = account;
+    await _saveAccounts();
     notifyListeners();
   }
 
   Future<void> deleteAccount(Account account) async {
     _accounts.removeWhere((a) => a.id == account.id);
+    await _saveAccounts();
     notifyListeners();
   }
 
-  Account findById(String id) {
-    return _accounts.firstWhere((a) => a.id == id, orElse: () => null);
+  Future<void> loadAccounts() async {
+    final preferences = await SharedPreferences.getInstance();
+    final jsonString = preferences.getString(_accountsPreferenceKey);
+    if (jsonString == null) return;
+
+    final jsonValue = json.decode(jsonString) as List<dynamic>;
+    _accounts = jsonValue.map((a) => Account.fromJson(a)).toList();
+    notifyListeners();
+  }
+
+  Future<void> _saveAccounts() async {
+    final preferences = await SharedPreferences.getInstance();
+    preferences.setString(_accountsPreferenceKey, json.encode(_accounts));
   }
 }
