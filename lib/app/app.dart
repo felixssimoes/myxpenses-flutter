61d2cb449a893e63dc7ad086d1eb6a760b1c8e85
Felix Simoes <felix.simoes@njiuko.com>
2019-09-29 16:22:39 +0200

    providers tests

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:myxpenses/app/app_navigator.dart';
 import 'package:myxpenses/config/locator.dart';
+import 'package:myxpenses/data/models/account.model.dart';
 import 'package:myxpenses/data/providers/accounts.provider.dart';
 import 'package:myxpenses/data/providers/date_interval.provider.dart';
 import 'package:myxpenses/data/providers/expenses.provider.dart';
+import 'package:myxpenses/data/repositories/accounts.repository.dart';
 import 'package:provider/provider.dart';
 
 class MyXpensesApp extends StatelessWidget {
@@ -21,8 +23,80 @@ class MyXpensesApp extends StatelessWidget {
           primarySwatch: Colors.teal,
           accentColor: Colors.teal[800],
         ),
-        routes: AppNavigator.setupRoutes(context),
-        navigatorKey: locator<AppNavigator>().rootNavigatorKey,
+        // routes: AppNavigator.setupRoutes(context),
+        // navigatorKey: locator<AppNavigator>().rootNavigatorKey,
+        home: Screen(),
+      ),
+    );
+  }
+}
+
+final repo = AccountsRepository();
+int count = 0;
+
+class Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          StreamProvider<Account>.value(
+            value: repo.accountById('123'),
+            child: Consumer<Account>(
+              builder: (c, a, _) {
+                print('show account stream provider/consumer');
+                return Center(child: Text(a?.name ?? 'no account'));
+              },
+            ),
+          ),
+          StreamBuilder<Account>(
+            stream: repo.accountById('123'),
+            builder: (_, AsyncSnapshot<Account> s) {
+              print('show account stream builder');
+              if (!s.hasData) {
+                return Text('no account yet');
+              }
+              return Text(s.data.name);
+            },
+          ),
+          RaisedButton(
+            child: Text('add account'),
+            onPressed: () {
+              count++;
+              repo.addAccount('name $count');
+            },
+          ),
+          StreamBuilder<Account>(
+            stream: repo.accountById('123'),
+            builder: (_, AsyncSnapshot<Account> s) {
+              print('edit account button');
+              if (!s.hasData) {
+                return Text('no account yet');
+              }
+              return RaisedButton(
+                child: Text('edit account'),
+                onPressed: () {
+                  count++;
+                  final a = s.data.copyWith(name: 'name $count');
+                  repo.updateAccount(a);
+                },
+              );
+            },
+          ),
+          StreamBuilder<List<Account>>(
+            stream: repo.accounts,
+            builder: (_, AsyncSnapshot<List<Account>> s) {
+              print('accounts list');
+              if (!s.hasData) {
+                return Text('no accounts yet');
+              }
+              return Column(
+                children: s.data.map((a) => Text(a.name)).toList(),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/data/repositories/accounts.repository.dart
+++ lib/data/repositories/accounts.repository.dart
@@ -1,6 +1,13 @@
 import 'package:myxpenses/data/models/account.model.dart';
 import 'package:rxdart/rxdart.dart';
 
+class AccountsStore {
+  final List<Account> _accounts = [];
+  List<Account> get accounts => _accounts;
+
+  Future<void> addAccount(Account account) async => _accounts.add(account);
+}
+
 class AccountsRepository {
   final _accounts = List<Account>();
 
@@ -12,19 +19,23 @@ class AccountsRepository {
 
   Observable<List<Account>> get accounts => _accountsList.stream;
 
-  // Observable<Account> accountById(String id) => _accountsList.value.firstWhere((a) => a.id == id);
   Observable<Account> accountById(String id) =>
-      _accountsList.stream.transform(ScanStreamTransformer((a, la, i) {
-        return la.firstWhere((a) => a.id == id);
-      }, null));
+      _accountsList.stream.map((la) => la.firstWhere((a) => a.id == id));
 
   Future<List<Account>> loadAccounts() async {
     return [];
   }
 
   Future<Account> addAccount(String name) async {
-    final account = Account(id: DateTime.now().toString(), name: name);
+    // final account = Account(id: DateTime.now().toString(), name: name);
+    final account = Account(id: '123', name: name);
     _accounts.add(account);
+    _accountsList.add(_accounts);
+    return account;
+  }
+
+  Future<Account> updateAccount(Account account) async {
+    _accounts[0] = account;
     _accountsList.add(_accounts);
     return account;
   }
